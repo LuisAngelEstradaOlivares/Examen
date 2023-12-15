e14f091e8ee1de1199b712a539f9520c81ce01d2
LuisAngelEstradaOlivares <153864448+LuisAngelEstradaOlivares@users.noreply.github.com>
2023-12-14 22:04:36 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+
+<title>Informacion Personal</title>
+
+</head>
+
+<body>
+
+<table>
+
+<tr>
+
+<td bgcolor="#FFA07A">Luis Angel</td> <!-- Light Salmon -->
+<td bgcolor="#00CED1">Estrada Olivares</td> <!-- Dark Turquoise -->
+<td bgcolor="#8A2BE2">21090995</td> <!-- Blue Violet -->
+
+</tr>
+
+</table>
+
+</body>
+
+</html>
